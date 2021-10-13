c3d2a55805c670dd673d035ffbf838df9be266a0
muzy06038 <sub06038@naver.com>
2021-10-14 01:25:50 +0900

    cctv on

--- src/components/Dashboard/CCTV.vue
+++ src/components/Dashboard/CCTV.vue
@@ -1,13 +1,34 @@
 <template>
   <div>CCTV 화면입니다.</div>
+  <div >
+    <canvas id="video" style="width: 640px; height: 480px;"></canvas>
+  </div>
 </template>
 
 <script>
+import JSMpeg from 'jsmpeg'
 export default {
     name: 'CCTV',
+  components: {},
+  data() {
+    return {
+    }
+  },
+  mounted() {
+      this.getVideos()
+  },
+
+  methods: {
+    getVideos(){
+      let canvas = document.getElementById('video');
+      let url = new WebSocket('ws://163.180.117.40:9999');
+      new JSMpeg(url, {canvas: canvas});
+
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
